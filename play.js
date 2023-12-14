536f03bc88ca2fb16db230c7b2f0b5084ce142fd
Abhishek Yadav <abhishek_yadav_work@outlook.com>
2023-12-14 13:49:43 +0530

    14-december

---
+++ play.js
@@ -0,0 +1,12 @@
+var name = "max";
+var age = 12;
+var hoby = "hobo";
+
+function summary(username,userage,userhoby){
+
+    return ("Name is" + name + "age is " + age + "hobby is " + hoby); 
+};
+
+
+console.log(summary(name,age,hoby));
+
