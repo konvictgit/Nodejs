f34f3dbd6c691383f8440028ef7a672c1ccde6a0
Abhishek Yadav <abhishek_yadav_work@outlook.com>
2023-12-15 16:23:28 +0530

    datatype and variable

---
+++ 01_variables.js
@@ -0,0 +1,7 @@
+//var account = "indian"
+const accountq = "india"
+let account1 = "noida"
+//account = "gaur"
+
+console.log(accountq);
+
---
+++ 02_datatype.js
@@ -0,0 +1,16 @@
+let name = "abhishek"
+let age = "";
+let boolena = true
+
+//number,bigint,string,boolean,null
+
+//null - standalone value , null is object in js
+//undefined - when value is not defined
+//symbol - unique
+
+
+
+
+//object 
+
+console.log(typeof age);
