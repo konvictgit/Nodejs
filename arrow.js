9ad0d54dc30adc31988e901e9f7de43d79b2eb8c
Abhishek Yadav <abhishek_yadav_work@outlook.com>
2023-12-19 16:20:53 +0530

    arrow2.0

--- arrow.js
+++ arrow.js
@@ -11,10 +11,40 @@ const usera = {
     }
 }
 
-usera.welcomemessage()
-usera.user = "lolu"  // created new context for this same user
-usera.welcomemessage()
+// usera.welcomemessage()
+// usera.user = "lolu"  // created new context for this same user
+// usera.welcomemessage()
 
 
 //console.log(this); will show empty {} because it is looking inside global context and its empty.
+// while on browser level terminal it(this) will show this.window prototype which contain multiple options access.
+
+// function chai() {
+//     let color = 'red';
+//     console.log(this.color);
+// }
+//when we try to access something to this inside from function i cannot access, it can only be access through object.
+//chai()
+
+const chai =  () => {
+    let username = "sasa"
+    console.log(this);
+}
+
+//chai()
+
+
+//Explicit return arrow fun
+
+// const addtwo = (num1, num2) => {
+//     return num1+num2
+// }
+
+// implicite return arrow fun
+// const addtow = (num1,num2) => (num1 + num2)
+
+// return obj in arrow
+const person = (name ,age )=> ({ name : name , age : age })
+
+console.log(person("a",3));
 
