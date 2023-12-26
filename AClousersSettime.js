def1aede1456bf4f90dc9399c188b96c5971a1be
Abhishek Yadav <abhishek_yadav_work@outlook.com>
2023-12-26 23:23:09 +0530

    reduce

--- AClousersSettime.js
+++ AClousersSettime.js
@@ -22,3 +22,19 @@ function x() {
 x();
 
 
+//// -- reduce -- \\\\\
+const namea = [
+    {firstname:"abc",lastname:"def",age:32},
+    {firstname:"eee",lastname:"rrr",age:22},
+    {firstname:"qqq",lastname:"rrr",age:55},
+]
+
+const output = namea.reduce(function (arr,curr) {
+    if (curr.age < 36) {
+        arr.push(curr.firstname)
+    }
+    return arr
+},[])
+
+console.log(output);
+
